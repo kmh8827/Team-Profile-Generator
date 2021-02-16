9caf5e031c43370ccf58e7a7f359350262e9898b
kevin <kevinhellman94@gmail.com>
2021-02-15 19:51:25 -0500

    Added a banner accross the top

--- index.js
+++ index.js
@@ -246,9 +246,12 @@ makePage = () => {
         <title>Team Profile Generator</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../Team-Profile-Generator/dist/style.css">
     </head>
     <body>
+    <header>
+        <h1>My Team</h1>
+    </header>
     <section>
     ${manager} ${interns} ${engineers}
     </section>
